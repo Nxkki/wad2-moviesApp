1a8c155637467dac20764f0a4c4723da701cb8b4
Nikki Osita <nikkio358@yahoo.com>
2020-11-03 17:08:39 +0000

    Show movie review excerpts; Refactor movie page

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,55 +1,126 @@
-import React, {useState, useEffect}  from "react";
-// import React from "react";
-import MovieHeader from "../components/headerMovie";
+import { Link, Route, withRouter } from "react-router-dom"
+import MovieReviews from "../components/movieReviews"
+import React, {useState, useEffect } from "react";
 import MovieDetails from "../components/movieDetails";
-import "./moviePage.css";
+import PageTemplate from "../components/templateMoviePage";
 import {getMovie} from '../api/tmdb-api'
 
 
 const MoviePage = props => {
-  const { id } = props.match.params
-  const [movie, setMovie] = useState(null)
+  const { id } = props.match.params;
+  const [movie, setMovie] = useState(null);
   useEffect(() => {
     getMovie(id).then(movie => {
       setMovie(movie);
     });
   }, [id]);
-  // useEffect(() => {
-  //   fetch(
-  //     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-  //   ).then(res => res.json())    
-  //   .then(movie => {
-  //     setMovie(movie);
-  //   })
-  // }, [id])
-// const MoviePage = ({ movie }) => {
+
   return (
     <>
-      {movie ? (
-        <>
-          <MovieHeader movie={movie} />
-          <div className="row">
-            <div className="col-3">
-              <img
-                src={
-                  movie.poster_path
-                    ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-                    : "./film-poster-placeholder.png"
-                }
-                className="movie"
-                alt={movie.title}
-              />
-            </div>
-            <div className="col-9">
-              <MovieDetails movie={movie} />
-            </div>
+    {movie ? (
+      <>
+        <PageTemplate movie={movie}>
+          <MovieDetails movie={movie} />
+        </PageTemplate>
+        <div className="row">
+          <div className="col-12 ">
+            {!props.history.location.pathname.endsWith("/reviews") ? (
+              <Link
+                className="btn btn-primary btn-block active"
+                to={`/movies/${id}/reviews`}
+              >
+                Show Reviews (Extracts)
+              </Link>
+            ) : (
+              <Link
+                className="btn btn-primary btn-block active"
+                to={`/movies/${id}`}
+              >
+                Hide Reviews 
+              </Link>
+            )}
           </div>
-        </>
-        ) : (
-        <p>Waiting for movie details</p>
-      )}
-    </>
+        </div>
+        <Route
+          path={`/movies/:id/reviews`}
+          render={props => <MovieReviews movie={movie} {...props} />}
+        />
+      </>
+    ) : (
+      <p>Waiting for movie details</p>
+    )}
+  </>
   );
 };
+  export default withRouter(MoviePage);
 
-export default MoviePage;
+//   return (
+//     <>
+//       {movie ? (
+//           <PageTemplate movie={movie}>
+//             <MovieDetails movie={movie} />
+//           </PageTemplate>
+//       ) : (
+//         <p>Waiting for movie details</p>
+//       )}
+//     </>
+//   );
+// };
+
+// export default MoviePage;
+
+// // import React, {useState, useEffect}  from "react";
+// // // import React from "react";
+// // import MovieHeader from "../components/headerMovie";
+// // import MovieDetails from "../components/movieDetails";
+// // import "./moviePage.css";
+// // import {getMovie} from '../api/tmdb-api'
+
+
+// // const MoviePage = props => {
+// //   const { id } = props.match.params
+// //   const [movie, setMovie] = useState(null)
+// //   useEffect(() => {
+// //     getMovie(id).then(movie => {
+// //       setMovie(movie);
+// //     });
+// //   }, [id]);
+// //   // useEffect(() => {
+// //   //   fetch(
+// //   //     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+// //   //   ).then(res => res.json())    
+// //   //   .then(movie => {
+// //   //     setMovie(movie);
+// //   //   })
+// //   // }, [id])
+// // // const MoviePage = ({ movie }) => {
+// //   return (
+// //     <>
+// //       {movie ? (
+// //         <>
+// //           <MovieHeader movie={movie} />
+// //           <div className="row">
+// //             <div className="col-3">
+// //               <img
+// //                 src={
+// //                   movie.poster_path
+// //                     ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+// //                     : "./film-poster-placeholder.png"
+// //                 }
+// //                 className="movie"
+// //                 alt={movie.title}
+// //               />
+// //             </div>
+// //             <div className="col-9">
+// //               <MovieDetails movie={movie} />
+// //             </div>
+// //           </div>
+// //         </>
+// //         ) : (
+// //         <p>Waiting for movie details</p>
+// //       )}
+// //     </>
+// //   );
+// // };
+
+// export default MoviePage
