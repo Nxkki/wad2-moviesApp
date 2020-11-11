c8c501b8e5107f5e75d950db6c894644ede4899d
Nikki Osita <nikkio358@yahoo.com>
2020-11-11 15:36:07 +0000

    Fix upcoming movies page

--- src/pages/upcomingMoviePage.js
+++ src/pages/upcomingMoviePage.js
@@ -1,35 +1,55 @@
-import React, { useState, useEffect } from "react";
-import StubAPI from "../api/stubAPI";
-import PageTemplate from "../components/templateMovieListPage";
-import { getMovies } from "../api/tmdb-api";
-// import useMovie from "../hooks/useMovie";
+import React, { useContext } from "react";
+import PageTemplate from '../components/templateMovieListPage'
+import {MoviesContext} from '../contexts/moviesContext'
+import AddToWatchListButton from '../components/buttons/addToWatchList'
 
 const MovieListPage = () => {
+  const context = useContext(MoviesContext);
 
-    const [movies, setMovies] = useState([]);
-    useEffect(() => {
-        getMovies().then(movies => {
-          setMovies(movies);
-        });
-      }, []);
-      const addToFavorites = movieId => {
-        setMovies(movies => {
-          const index = movies.map(m => m.id).indexOf(movieId);
-          StubAPI.add(movies[index]);
-          let newMoviesState = [...movies]
-          newMoviesState.splice(index, 1);
-          return newMoviesState;
-        });
-      };
+  return (
+      <PageTemplate 
+        title='No. Movies'
+        movies={context.movies}
+        action={(movie) => {
+          return <AddToWatchListButton movie={movie} /> 
+        }}
+      />
+  );
+};
+
+export default MovieListPage;
+// import React, { useState, useEffect } from "react";
+// import StubAPI from "../api/stubAPI";
+// import PageTemplate from "../components/templateMovieListPage";
+// import { getMovies } from "../api/tmdb-api";
+// // import useMovie from "../hooks/useMovie";
+
+// const MovieListPage = () => {
+
+//     const [movies, setMovies] = useState([]);
+//     useEffect(() => {
+//         getMovies().then(movies => {
+//           setMovies(movies);
+//         });
+//       }, []);
+//       const addToFavorites = movieId => {
+//         setMovies(movies => {
+//           const index = movies.map(m => m.id).indexOf(movieId);
+//           StubAPI.add(movies[index]);
+//           let newMoviesState = [...movies]
+//           newMoviesState.splice(index, 1);
+//           return newMoviesState;
+//         });
+//       };
     
-      return (
-          <PageTemplate
-            title='Discover Movies'
-            movies={movies}
-            buttonHandler={addToFavorites}
-          />
-      );
-    };
+//       return (
+//           <PageTemplate
+//             title='Discover Movies'
+//             movies={movies}
+//             buttonHandler={addToFavorites}
+//           />
+//       );
+//     };
     
-    export default MovieListPage;
+//     export default MovieListPage;
     
